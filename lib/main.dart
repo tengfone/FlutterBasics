7bc38aa281a3b7c0db0842deba4f465f66bbf7e8
Teng Fone <phangtf@gmail.com>
2020-05-12 16:27:49 +0800

    Iterate Through List

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,37 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
-  home: IDCard(),
+  home: QuoteList(),
 ));
 
-class IDCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _IDCardState createState() => _IDCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _IDCardState extends State<IDCard> {
+class _QuoteListState extends State<QuoteList> {
 
-  int counter = 0;
+  List<Quote> quotes = [
+    Quote(author: 'TengFone', text: "Hello"),
+    Quote(author: "TengFone", text: "First Attempt At Flutter"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("ID Card"),
+        title: Text("Quote List"),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
+        backgroundColor: Colors.redAccent,
       ),
-      // Increment Button
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            counter++;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30,40,30,0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/iloveyoupatric.jpg'),
-                radius: 40,
-              ),
-            ),
-            Divider(
-              height: 90.0,
-              color: Colors.blue[800],
-              thickness: 3.0,
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing:  2.0,
-              )
-            ),
-            SizedBox(height: 10.0),
-            Text(
-                "TengFone",
-                style: TextStyle(
-                  color: Colors.amberAccent[100],
-                  letterSpacing:  2.0,
-                  fontFamily: 'customFont',
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-                "Phone Number",
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing:  2.0,
-                )
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "XXXX XXXX",
-              style: TextStyle(
-                color: Colors.amberAccent[100],
-                letterSpacing:  2.0,
-                fontFamily: 'customFont',
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-                "Counter",
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing:  2.0,
-                )
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "$counter",
-              style: TextStyle(
-                color: Colors.amberAccent[100],
-                letterSpacing:  2.0,
-                fontFamily: 'customFont',
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  "randomEmail@gmail.com",
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  )
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: Column(
+        // children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((i) {
+          return Text('${i.text} - ${i.author}');
+        }).toList(),
+      )
     );
   }
 }
---
+++ lib/quote.dart
@@ -0,0 +1,11 @@
+class Quote{
+
+  String text;
+  String author;
+
+  // named parameters, can be done in any orders
+  Quote({this.text,this.author});
+
+}
+
+// Quote myquote = Quote(author: "TengFone", text: "Hello")
