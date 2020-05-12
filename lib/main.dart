51edf0528f80c2dab1f08ca6fdb7c1ac00ecfdb5
Teng Fone <phangtf@gmail.com>
2020-05-12 13:32:25 +0800

    Basic Rows

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,22 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green[100],
       ),
-      body: Container(
-        // padding is inside the container
-        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30, 40),
-        // margin is around the container
-        margin: EdgeInsets.all(30.0),
-        // padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 10.0),
-        color: Colors.grey[300],
-        child: Text("Hello World"),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text("Hello World"),
+          FlatButton(
+            onPressed: (){},
+            color: Colors.amber,
+            child:Text("Button"),),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text("In Container"),
+          )
+        ],
       ),
-//      // Use Padding widget when you only want to pad stuff. Use container if
-//      // you want to use margin
-//      body: Padding(
-//        padding: EdgeInsets.all(30.0),
-//        child: Text("Hello World!")
-//      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: Text("Click"),
