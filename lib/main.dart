cf7a6870342249ac960279519824487d217b1877
Teng Fone <phangtf@gmail.com>
2020-05-12 14:33:49 +0800

    First Sample ID Card Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: IDCard(),
 ));
 
-class Home extends StatelessWidget {
+class IDCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Making A stateless Widget
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("TopAppBar"),
+        title: Text("ID Card"),
         centerTitle: true,
-        backgroundColor: Colors.green[100],
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
-      body:Row(
-        children: <Widget>[
-          Expanded(
-            // Image Resizer
-            flex: 2,
-              child: Image.asset("assets/iloveyoupatric.jpg")
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.green,
-              child: Text("1"),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30,40,30,0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/iloveyoupatric.jpg'),
+                radius: 40,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.pinkAccent,
-              child: Text("2"),
+            Divider(
+              height: 90.0,
+              color: Colors.blue[800],
+              thickness: 3.0,
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.amber,
-              child: Text("3"),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing:  2.0,
+              )
             ),
-          )
-        ],
-    ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Text("Click"),
-        backgroundColor: Colors.red[400],
+            SizedBox(height: 10.0),
+            Text(
+                "TengFone",
+                style: TextStyle(
+                  color: Colors.amberAccent[100],
+                  letterSpacing:  2.0,
+                  fontFamily: 'customFont',
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+                "Phone Number",
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing:  2.0,
+                )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "XXXX XXXX",
+              style: TextStyle(
+                color: Colors.amberAccent[100],
+                letterSpacing:  2.0,
+                fontFamily: 'customFont',
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "randomEmail@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  )
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
